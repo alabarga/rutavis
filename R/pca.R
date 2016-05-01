96b673b10fadc55cf042b4d4af71747e87510583
David Charte <fdavidcl@outlook.com>
2016-05-01 20:06:49 +0200

    PCA implementation

---
+++ R/pca.R
@@ -0,0 +1,34 @@
+#' @export
+newModel.pca <- function(dataset, class_col = length(dataset), dimensions, name, ...) {
+  inputs <- dataset[-class_col]
+
+  if (!all(sapply(inputs, function(col) "numeric" %in% class(col))))
+    stop("dlvis: Input columns must be numeric for PCA to work")
+
+  # Principal Components Analysis
+  results <- stats::prcomp(dataset[-class_col], center = TRUE, scale = FALSE, ...)
+  dlmodel <- list(
+    model = results$x[, 1:dimensions],
+    classes = dataset[class_col],
+    name = name
+  )
+  class(dlmodel) <- "dlmodel"
+  dlmodel
+
+  # # Truncate to specified dimension number
+  # truncated <- results$x[, 1:dimensions] %*% t(results$rotation[, 1:dimensions])
+  # print(dim(results$x[, 1:dimensions]))
+  # print(dim(results$rotation[, 1:dimensions]))
+  # print(dim(truncated))
+  #
+  # # Rescale and restore center to data
+  # if (any(results$scale != FALSE)) {
+  # 	truncated <- scale(truncated, center = FALSE, scale = 1/results$scale)
+  # }
+  # if (any(results$center != FALSE)) {
+  #   truncated <- scale(truncated, center = -1 * results$center, scale = FALSE)
+  # }
+  # print(dim(dataset))
+  #
+  # truncated
+}
